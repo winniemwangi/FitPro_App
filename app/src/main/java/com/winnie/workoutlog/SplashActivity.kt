fe0acfe1df47de369a8e0c0269376b1312806b57
winniemwangi <winniewameremwangi@gmail.com>
2022-06-02 17:32:49 +0300

    added splash screen activity

--- app/src/main/java/com/winnie/workoutlog/SplashActivity.kt
+++ app/src/main/java/com/winnie/workoutlog/SplashActivity.kt
@@ -6,6 +6,5 @@ import android.os.Bundle
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
